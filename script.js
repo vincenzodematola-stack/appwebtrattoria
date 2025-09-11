44c47e8018cf4be9fa4ad96471caca8812b9dbf4
vincenzodematola-stack <vincenzodematola@gmail.com>
2025-09-11 20:19:58 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAm-0lgqE-ecZ4LJ_pn3sgDmDaQPId6X4E",
-  authDomain: "comande-digitali.firebaseapp.com",
-  projectId: "comande-digitali",
-  storageBucket: "comande-digitali.firebasestorage.app",
-  messagingSenderId: "831913671987",
-  appId: "1:831913671987:web:e5035df8e47bea69331246",
-  measurementId: "G-RRJ4RC7YPG"
-};
