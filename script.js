1769189cb09697892df9e554caa8fc894484ebe3
vincenzodematola-stack <vincenzodematola@gmail.com>
2025-09-16 09:39:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+<script>
+      // La tua configurazione di Firebase
+      const firebaseConfig = {
+        apiKey: "AIzaSyAm-0lgqE-ecZ4LJ_pn3sgDmDaQPId6X4E",
+        authDomain: "comande-digitali.firebaseapp.com",
+        projectId: "comande-digitali",
+        storageBucket: "comande-digitali.firebasestorage.app",
+        messagingSenderId: "831913671987",
+        appId: "1:831913671987:web:e5035df8e47bea69331246",
+        measurementId: "G-RRJ4RC7YPG"
+      };
+
+      // Inizializza Firebase
+      firebase.initializeApp(firebaseConfig);
+    </script>
 // Inizializza il database di Firebase
 const database = firebase.database();
 const ordiniRef = database.ref('ordini');
